d210b8796f828a9ca31c91d4d02f193060a64899
Pavel Prokofiev <integral.pro@gmail.com>
2011-12-20 21:46:35 +0600

    Added GlobalProperties stubs

--- src/org/munta/model/GlobalProperties.java
+++ src/org/munta/model/GlobalProperties.java
@@ -5,6 +5,13 @@ import java.io.Serializable;
 public class GlobalProperties implements Serializable {
 
     public int test = 1123;
+    
+    /// Block 1 start
+    /// Block 1 end
+    
+    /// Block 2 start
+    public int mytest = 9999;
+    /// Block 2 end
 
     public void clear() {
     }
