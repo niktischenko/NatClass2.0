feb1e3fe55b7fe5ec99db832cea4b2031e44717b
Pavel Prokofiev <integral.pro@gmail.com>
2011-12-20 18:23:03 +0700

    Removed sucks JAXB parser annotations

---
+++ src/org/munta/projectengine/ObjectSerializer.java
@@ -0,0 +1,37 @@
+package org.munta.projectengine;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ObjectSerializer implements IProjectSerializer {
+
+    @Override
+    public void serializeProjectObject(Object o, OutputStream w) throws SerializerException {
+        ObjectOutputStream oos = null;
+        try {
+            oos = new ObjectOutputStream(w);
+            oos.writeObject(o);
+        } catch (IOException ex) {
+            Logger.getLogger(ObjectSerializer.class.getName()).log(Level.SEVERE, null, ex);
+            throw new SerializerException(ex);
+        }
+    }
+
+    @Override
+    public Object deserializeProjectObject(InputStream r) throws SerializerException {
+        ObjectInputStream ois = null;
+        try {
+            ois = new ObjectInputStream(r);
+            Object o = ois.readObject();
+            return o;
+        } catch (Exception ex) {
+            Logger.getLogger(ObjectSerializer.class.getName()).log(Level.SEVERE, null, ex);
+            throw new SerializerException(ex);
+        }
+    }
+}
